397fb329e8c4e693ed2ad6513bb0b39d08a4aea8
Yon Ploj <admin@yon.si>
2023-05-26 00:40:52 +0200

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,10 @@
+# avtonet-scraper.py
+
+"""
+.GO-Results-Row
+	a.stretched-link.href -> .col-12.p-3.GO-Rounded.innertext
+	.GO-Results-Naziv.innertext
+	.GO-Results-Price.innertext
+	table.table
+"""
+# TODO: make scraper
